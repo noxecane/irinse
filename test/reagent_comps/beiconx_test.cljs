a8a3a66a206dbfad630b8666e6e6df3ad60a35ad
Arewa Olakunle <danceasarxx@gmail.com>
2017-05-05 05:17:07 +0100

    convert stream to reagent atom (#6)
    Wrote 2 variants of x-ratom that writes stream values directly to
    the atom or reduces/folds those values over the atom.

--- test/reagent_comps/beiconx_test.cljs
+++ test/reagent_comps/beiconx_test.cljs
@@ -1,15 +1,31 @@
 (ns reagent-comps.beiconx-test
   (:require [beicon.core :as rx]
             [reagent-comps.beiconx :as sut]
-            [cljs.test :as t :refer-macros [deftest async is]]))
+            [cljs.test :as t :refer-macros [deftest async is testing]]))
 
 
 (deftest test-factory
   (async done
-   (let [[f v$] (sut/factory)]
-     (rx/on-value v$ #(is (<= % 10)))
-     (f 10)
-     (f 9)
-     (f 5)
-     (f 1)
+    (let [[f v$] (sut/factory)
+          sub    (rx/on-value v$ #(is (<= % 10)))]
+     (doseq [x [1 2 4 59 10]]
+       (f x))
+     (rx/cancel! sub)
      (done))))
+
+
+(deftest test-converter
+  (testing "Simple values"
+    (let [[f v$] (sut/factory)
+          state  (sut/x->ratom "" v$)]
+      (f "First")
+      (f "Latest")
+      (is (= "Latest" @state))))
+
+  (testing "Using fold"
+    (let [[f v$] (sut/factory)
+          state  (sut/x->ratom [] conj v$)]
+      (doseq [x (range 10)]
+        (f x))
+      (is (-> @state count (= 10))))))
+
