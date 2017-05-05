306a58a0cb193fca65f659a5fe519be0c99a96f3
Arewa Olakunle <danceasarxx@gmail.com>
2017-05-05 17:15:36 +0100

    No function level docs
    Added documentation for the three functions. Also added an overload
    for `x-ratom` for creating ratoms without initial value.

--- src/reagent_comps/beiconx.cljs
+++ src/reagent_comps/beiconx.cljs
@@ -5,12 +5,20 @@
             [cljs.core.async :as a]))
 
 
-(defn factory []
+(defn factory
+  "Create a triple of an observable, its push function
+   and a termination function."
+  []
   (let [sub      (rx/subject)]
     [sub #(rx/push! sub %) #(rx/end! sub)]))
 
 
 (defn x->ratom
+  "Convert an observable to a reagent atom. Can accept a fold to
+   reduce over the state rather than write directly to it."
+  ([stream]
+   (x->ratom nil stream))
+
   ([initial-value stream]
    (let [state (r/atom initial-value)
          sub   (rx/subscribe stream #(reset! state %))]
@@ -22,7 +30,10 @@
      state)))
 
 
-(defn from-chan [ch]
+(defn from-chan
+  "Creates an observable consuming events from the given core.async
+   channel."
+  [ch]
   (let [[v f c] (factory)]
     (go-loop [x (a/<! ch)]
       (if (some? x) (f x) (c)))
