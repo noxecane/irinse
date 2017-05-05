fcecbbf573b438e337509135d2e62d553b67a5ad
Arewa Olakunle <danceasarxx@gmail.com>
2017-05-05 06:59:01 +0100

    async operations in stream
    Some libraries return core.async channels for async operations.
    So I create a from-chan function that makes it possible to retrieve
    the value(s) from the channel and completes when the channel closes.

--- test/reagent_comps/beiconx_test.cljs
+++ test/reagent_comps/beiconx_test.cljs
@@ -1,31 +1,43 @@
 (ns reagent-comps.beiconx-test
   (:require [beicon.core :as rx]
             [reagent-comps.beiconx :as sut]
-            [cljs.test :as t :refer-macros [deftest async is testing]]))
+            [cljs.test :as t :refer-macros [deftest async is testing]]
+            [cljs.core.async :as a]))
 
 
 (deftest test-factory
   (async done
-    (let [[f v$] (sut/factory)
-          sub    (rx/on-value v$ #(is (<= % 10)))]
-     (doseq [x [1 2 4 59 10]]
-       (f x))
-     (rx/cancel! sub)
-     (done))))
+         (let [[v f c] (sut/factory)
+               sub     (rx/on-value v #(is (<= % 10)))]
+           (doseq [x [1 2 4 5 9 10]]
+             (f x))
+           (c)
+           (done))))
 
 
 (deftest test-converter
   (testing "Simple values"
-    (let [[f v$] (sut/factory)
-          state  (sut/x->ratom "" v$)]
+    (let [[v f c] (sut/factory)
+          state   (sut/x->ratom "" v)]
       (f "First")
       (f "Latest")
-      (is (= "Latest" @state))))
+      (is (= "Latest" @state))
+      (c)))
 
   (testing "Using fold"
-    (let [[f v$] (sut/factory)
-          state  (sut/x->ratom [] conj v$)]
+    (let [[v f c] (sut/factory)
+          state   (sut/x->ratom [] conj v)]
       (doseq [x (range 10)]
         (f x))
-      (is (-> @state count (= 10))))))
+      (is (-> @state count (= 10)))
+      (c))))
 
+
+(deftest test-channel
+  (testing "Single Dispatch"
+    (async done
+           (let [e   (a/chan 1)
+                 v   (sut/from-chan e)
+                 sub (rx/on-value v #(is (= % 10)))]
+             (a/put! e 10)
+             (done)))))
