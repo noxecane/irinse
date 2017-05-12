06c7b96f58ddaf776a4c46f0a93770e358b341cf
Arewa Olakunle <danceasarxx@gmail.com>
2017-05-12 12:50:04 +0100

    autoselect does not following modal in using bulma
    Added input class to autoselect's input element.

--- examples/example/core.cljs
+++ examples/example/core.cljs
@@ -3,41 +3,6 @@
             [reagent-comps.forms.autoselect :as autoselect]))
 
 
-(def languages
-  [["Clojure" :hard]
-   ["Java" :complex]
-   ["Javascript" :messy]
-   ["PHP" :ugly]
-   ["Python" :popular]
-   ["Haskell" :esoteric]
-   ["Bash" :dirty]
-   ["C" :practical]
-   ["AWK" :proper]])
-
-
-(defn find-lang [name]
-  (letfn [(lang? [[lang-name :as l]]
-            (let [n (clojure.string/lower-case name)
-                  ln (clojure.string/lower-case lang-name)]
-              (clojure.string/starts-with? ln n)))]
-    (filter lang? languages)))
-
-
-(defn render [[name type]]
-  [:div.lang
-   [:p.name name]
-   [:small (str type)]])
-
-
-(defn lang-auto []
-  [autoselect/view
-   (autoselect/model {:suggest-fn find-lang
-                      :on-select  #(println "Selected" %)})
-   {:show   first
-    :render render}])
-
-
 (r/render-component
- [:div
-  [lang-auto]]
+ [:div]
  (. js/document (getElementById "app")))
