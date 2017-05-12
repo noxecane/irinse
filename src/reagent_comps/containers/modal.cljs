f56512f54a86ff92de495a437fcbcb23e3ebf847
Arewa Olakunle <danceasarxx@gmail.com>
2017-05-12 12:57:40 +0100

    creating dialog boxes (#8)
    Added two components. A modal with external close button and a
    modal with internal close button. The latter has more structure
    than the former.

---
+++ src/reagent_comps/containers/modal.cljs
@@ -0,0 +1,32 @@
+(ns reagent-comps.containers.modal
+  (:require [beicon.core :as rx]
+            [reagent-comps.beiconx :as rxt]))
+
+
+(defn view
+  [{:keys [trigger]} & children]
+  (let [open? (rxt/to-ratom false trigger)]
+    (fn []
+      [:div.modal (if @open? "is-active" "")
+       [:div.modal-background {:on-click #(trigger false)}]
+       (into [:div.modal-content] children)
+       [:button.modal-close {:on-click #(trigger false)}]])))
+
+
+(defn modal-card-footer [& children]
+  (into [:footer.modal-card-foot] footer-children))
+
+
+(defn view-card
+  [{:keys [trigger]}
+   {:keys [title footer]}
+   & children]
+  (let [open? (rxt/to-ratom false trigger)]
+    [:div.modal
+     [:div.modal-background {:on-click #(trigger false)}]
+     [:div.modal-card
+      [:header.modal-card-head
+       [:p.modal-card-title title]
+       [:button.delete {:on-click #(trigger false)}]]
+      (into [:div.modal-card-body] children)
+      footer]]))
